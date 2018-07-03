763a499acb5f93e09810beeebb86ee7f337474a7
Vegard Ingebrigtsen <Vegard@proventus.no>
2018-07-03 09:33:30 +0200

    lel

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -10,7 +10,19 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello, " + name);
+
+            if(hoursOfSleep > 8) {
+                Console.WriteLine("Your are well rested");
+            } else {
+                Console.WriteLine("You need more sleep");
+            }
         }
     }
 }
