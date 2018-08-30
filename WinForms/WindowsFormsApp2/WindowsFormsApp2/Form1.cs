ec5093137ac51763ff9a4db35ff622852f7bf90c
Vegard Ingebrigtsen <Vegard@proventus.no>
2018-08-30 09:58:54 +0200

    Added rider project

---
+++ WinForms/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK) {
+                pictureBox1.BackColor = colorDialog1.Color;
+            }
+        }
+    }
+}
